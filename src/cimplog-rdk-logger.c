57090497764ab19676a30c478f1224d1ca82ede4
Ramki Ananthakrishnan <ramki_ananthakrishnan@cable.comcast.com>
2017-02-08 16:36:26 -0800

    Applied feeback from review.

--- src/cimplog-rdk-logger.c
+++ src/cimplog-rdk-logger.c
@@ -9,16 +9,15 @@
 #define MAX_BUF_SIZE 1024
 #define DEFAULT_MODULE "NO MODULE"
 
-static bool __rdk_logger_init = false;
-
-char *__attribute__((weak)) rdk_logger_module_fetch(void);
+const char *__attribute__((weak)) rdk_logger_module_fetch(void);
 
 void __cimplog(const char *module, int level, const char *msg, ...)
 {
     static const rdk_LogLevel _level[] = { RDK_LOG_ERROR, RDK_LOG_INFO, RDK_LOG_DEBUG };
+    static bool __rdk_logger_init = false;
     va_list arg_ptr;
     char buf[MAX_BUF_SIZE];
-    char *rdk_logger_module = NULL;
+    const char *rdk_logger_module = NULL;
     int nbytes;
 
     if( false == __rdk_logger_init )
@@ -41,7 +40,7 @@ void __cimplog(const char *module, int level, const char *msg, ...)
     RDK_LOG(_level[0x2 & level], rdk_logger_module, "%s: %s", module, buf);
 }
 
-char *rdk_logger_module_fetch(void)
+const char *rdk_logger_module_fetch(void)
 {
     return (DEFAULT_MODULE);
 }
