3368f533bd723a93b51f1eb4c6d8203646afc076
Ramki Ananthakrishnan <ramki_ananthakrishnan@cable.comcast.com>
2017-02-08 18:09:02 -0800

    Applied more code review feedback fixes and changed order of lcov call for tests.

--- src/cimplog-rdk-logger.c
+++ src/cimplog-rdk-logger.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 #include <rdk_debug.h>
 
 #include "cimplog.h"
@@ -13,13 +12,12 @@ const char *__attribute__((weak)) rdk_logger_module_fetch(void);
 void __cimplog(const char *module, int level, const char *msg, ...)
 {
     static const rdk_LogLevel _level[] = { RDK_LOG_ERROR, RDK_LOG_INFO, RDK_LOG_DEBUG };
-    static bool __rdk_logger_init = false;
     va_list arg_ptr;
     char buf[MAX_BUF_SIZE];
     const char *rdk_logger_module = NULL;
     int nbytes;
 
-    if( false == __rdk_logger_init )
+    if( NULL == rdk_logger_module )
     {
         rdk_logger_init("/etc/debug.ini");
         rdk_logger_module = rdk_logger_module_fetch();
@@ -28,7 +26,6 @@ void __cimplog(const char *module, int level, const char *msg, ...)
             fprintf(stderr, "\nERROR: Unable to initialize RDK logging!!!\n");
             exit(0);
         }
-        __rdk_logger_init = true;
     }
 
     va_start(arg_ptr, msg);
