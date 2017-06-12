831bd48e59752be1889df1b6869e0e5acdf8ab70
Murugan Viswanathan <murugan.viswanathan2@cognizant.com>
2017-06-12 14:56:31 -0700

    Fix for scenario when RDK logger not integrated

--- src/cimplog-rdk-logger.c
+++ src/cimplog-rdk-logger.c
@@ -11,24 +11,34 @@ const char *__attribute__((weak)) rdk_logger_module_fetch(void);
 
 void __cimplog(const char *module, int level, const char *msg, ...)
 {
-    static const rdk_LogLevel _level[] = { RDK_LOG_ERROR, RDK_LOG_INFO, RDK_LOG_DEBUG };
     static const char *rdk_logger_module = NULL;
+    static int init_done = 0;
+
+    if( !init_done )
+    {
+        rdk_logger_init("/etc/debug.ini");
+        rdk_logger_module = rdk_logger_module_fetch();
+        if( NULL == rdk_logger_module )
+        {
+            fprintf(stderr, "\nERROR: RDK Logger not integrated for this module !!!\n");
+            fprintf(stderr, " Provide cimplog method \"const char *rdk_logger_module_fetch(void)\" to get log prints !!\n");
+        }
+        init_done = 1;
+    }
+
+    if( NULL == rdk_logger_module )
+    {
+        //if RDK logger module is not defined, simple return - dont print.
+        return;
+    }
+
+    //else print to RDK Logger
+    static const rdk_LogLevel _level[] = { RDK_LOG_ERROR, RDK_LOG_INFO, RDK_LOG_DEBUG };
     va_list arg_ptr;
     char buf[MAX_BUF_SIZE];
     int nbytes;
 
-    if( NULL == rdk_logger_module )
-    {
-        rdk_logger_init("/etc/debug.ini");
-        rdk_logger_module = rdk_logger_module_fetch();
-	if( NULL == rdk_logger_module )
-        {
-            fprintf(stderr, "\nERROR: Unable to initialize RDK logging!!!\n");
-            exit(0);
-        }
-    }
-     
-	if (level <= LEVEL_INFO)
+    if (level <= LEVEL_INFO)
     {
 		va_start(arg_ptr, msg);
 		nbytes = vsnprintf(buf, MAX_BUF_SIZE, msg, arg_ptr);
@@ -43,11 +53,13 @@ void __cimplog(const char *module, int level, const char *msg, ...)
 			buf[nbytes] = '\0';
 		}
 		
-
 		RDK_LOG(_level[0x3 & level], rdk_logger_module, "%s: %s", module, buf);
     }
+
+    return;
 }
 
+// The below 'weak' linkage should be replaced with strong definition in the module integrating to cimplog
 const char *rdk_logger_module_fetch(void)
 {
     return NULL;
